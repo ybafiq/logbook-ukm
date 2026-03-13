73fd6f64ff4884c60b1e0b1c8acad4c788d058ee
afiqakmal <afiqakmal226@gmail.com>
2026-03-13 11:19:05 +0800

    fixing bugs in new  structure for database

--- database/migrations/2025_10_13_013018_create_stbc4866_entries_table.php
+++ database/migrations/2025_10_13_013018_create_stbc4866_entries_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('stbc4866_entries');
         Schema::create('stbc4866_entries', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
--- database/migrations/2025_10_13_061352_create_stbc4966_entries_table.php
+++ database/migrations/2025_10_13_061352_create_stbc4966_entries_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('stbc4966_entries');
         Schema::create('stbc4966_entries', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
